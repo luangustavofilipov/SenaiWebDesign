4de0404135f337910095e8bc9e15da7d686f7a48
Luan Gustavo Altruda Filipov <luan.gustavo.filipov@gmail.com>
2023-06-04 08:07:09 -0300

    First commit

---
+++ 25_Calculo_1/main.js
@@ -0,0 +1,8 @@
+// Declaração das variáveis
+let numero1 = Number(prompt("Informe o primeiro número: "))
+let numero2 = Number(prompt("Informe o segundo número: "))
+
+let soma = numero1 + numero2
+
+document.getElementById("resultado").innerHTML = soma
+console.log(soma)
